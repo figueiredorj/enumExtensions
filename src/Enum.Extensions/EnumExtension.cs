4626f4a137b0cd9f47be29e70dc903ac30aab8cd
Ricardo Figueiredo <figueiredorj@gmail.com>
2021-08-17 18:01:57 +0100

    enum extensions for converting to string from a description and parse it

---
+++ src/Enum.Extensions/EnumExtension.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+
+namespace Enum.Extensions
+{
+    public static class EnumExtension
+    {
+        public static string ToDescription<T>(this T enumerationValue)
+            where T : struct
+        {
+
+            string description = enumerationValue.ToString();
+
+            Type type = enumerationValue.GetType();
+            if (!type.IsEnum)
+            {
+                throw new ArgumentException("EnumerationValue must be of Enum type", "enumerationValue");
+            }
+
+            //Tries to find a DescriptionAttribute for a potential friendly name
+            //for the enum
+            MemberInfo[] memberInfo = type.GetMember(enumerationValue.ToString());
+            if (memberInfo != null && memberInfo.Length > 0)
+            {
+                object[] attrs = memberInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                if (attrs != null && attrs.Length > 0)
+                {
+                    //Pull out the description value
+                    description = ((DescriptionAttribute)attrs[0]).Description;
+                }
+            }
+            //If we have no description attribute, just return the ToString of the enum
+            return description;
+        }
+
+        public static T EnumFromDescription<T>(string description) where T : System.Enum
+        {
+            T enumValue = default(T);
+            bool found = false;
+
+            foreach (var field in typeof(T).GetFields().Where(hasDescriptionAttribute).AsEnumerable())
+            {
+                if (!found && Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) is DescriptionAttribute attribute)
+                {
+                    if (attribute.Description == description)
+                    {
+                        enumValue = (T)field.GetValue(null);
+                        found = true;
+                    }
+
+                }
+                else
+                {
+                    if (field.Name == description)
+                    {
+                        enumValue = (T)field.GetValue(null);
+                        found = true;
+                    }
+
+                }
+            }
+
+            if (enumValue == null) throw new Exception($"No value in defined enum of type {typeof(T).FullName}");
+
+            return enumValue;
+        }
+
+        private static bool hasDescriptionAttribute(FieldInfo arg)
+        {
+            return arg.GetCustomAttributes(typeof(DescriptionAttribute)).Any();
+        }
+    }
+}
---
+++ src/Enums.App/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using Enum.Extensions;
+
+namespace Enums.App
+{
+    class Program
+    {
+        public enum EnumDataTest
+        {
+            [Description("undefined value")]
+            undefined = 0,
+
+            [Description("unset value")]
+            unset = 1,
+
+            [Description("value was set")]
+            set = 2
+        }
+        static void Main(string[] args)
+        {
+            List<string> enumDescription = new List<string>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                var enumData = (EnumDataTest)i;
+                enumDescription.Add(enumData.ToDescription<EnumDataTest>());
+            }
+
+            List<EnumDataTest> enumValue = new List<EnumDataTest>();
+
+            foreach (string enumDataTest in enumDescription)
+            {
+                EnumDataTest value = EnumExtension.EnumFromDescription<EnumDataTest>(enumDataTest);
+                enumValue.Add(value);
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                  var enumData = (EnumDataTest)i;
+                  Console.WriteLine($"{enumData.ToDescription()} == {enumValue[i]}");
+            }
+
+        }
+
+
+    }
+
+}
